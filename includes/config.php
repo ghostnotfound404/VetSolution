e2b16fd2f153226f22d127d040ceb4f0f36d40c2
SebasSilvaT <126924256+SebasSilvaT@users.noreply.github.com>
2025-08-01 18:45:07 -0500

    Actualizar configuración de conexión a la base de datos y mejorar la eliminación de clientes

--- includes/config.php
+++ includes/config.php
@@ -2,10 +2,11 @@
 define('DB_HOST', 'localhost');      // Dirección del servidor MySQL (usualmente 'localhost')
 define('DB_USER', 'root');           // Usuario de la base de datos
 define('DB_PASS', '');              // Contraseña del usuario de la base de datos
+define('DB_PORT', 3307);              // Contraseña del usuario de la base de datos
 define('DB_NAME', 'vetapp_db');    // Nombre de la base de datos
 
 // Crear la conexión
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
 // Comprobar la conexión
 if ($conn->connect_error) {
